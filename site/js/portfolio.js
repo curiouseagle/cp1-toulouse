2e565f428e77a0408ab8b1353f2e60f28062b543
curiouseagle <anais.maligebordes@gmail.com>
2022-10-10 17:25:07 +0200

    correction

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,15 +5,33 @@ userAvatar.addEventListener('click', function () {
   userAvatar.src = "image/avatar.svg";
 });
 
+// const changeName = document.querySelector('.change-name');
+// const userName = document.getElementById("name");
+// const bgDescription = document.querySelector('.pink-bg');
+
+// changeName.addEventListener('click', () =>{
+//     const color = prompt("Choose a color");
+//     bgDescription.style.backgroundColor = color;
+//     userName.style.color = "white";
+//     userName.innerHTML = prompt("what's your name ?");
+// } )
+
+
 const changeName = document.querySelector('.change-name');
 const userName = document.getElementById("name");
-const bgDescription = document.querySelector('.pink-bg');
+const changeColor = document.querySelectorAll('.pink-bg, .pink-text');
 
-changeName.addEventListener('click', function () {
-    const color = prompt("Choose a color");
-    bgDescription.style.backgroundColor = color;
-    
+
+changeName.addEventListener('click', () =>{
     userName.innerHTML = prompt("what's your name ?");
     userName.style.color = "white";
-} )
+    const color = prompt("Choose a color");
 
+    changeColor.forEach(element => {
+        if (element.classList.contains('pink-bg')){
+            element.style.backgroundColor = color;
+        } else {
+            element.style.color = color;
+       }
+   })
+} )
